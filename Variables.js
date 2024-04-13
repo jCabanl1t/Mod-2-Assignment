f1d05b65115113d9e95da6c6af39e5328e648e30
Joseph Cabanlit III <JosephCabanlit@student.olympic.edu>
2024-04-13 14:26:33 -0700

    added variables html and js

---
+++ Variables.js
@@ -0,0 +1,34 @@
+/* 3 Examples of basic arithmetic in JS */
+/* console.log(9 + 10);
+console.log(5 * 6);
+console.log(64 / 8);
+
+/* 2 Examples of declaring variables in JS */
+/* let message;
+message = "Hello world!";
+
+alert(message); */
+
+/*let text = "good after-morning.";
+let hello;
+hello = text;
+
+alert(text);
+alert(hello); */
+
+/* 2 Examples of using variables in arithmetic operations in JS */
+/*let x;
+x = 5 + 4;
+
+console.log(x); */
+
+let x;
+x = 10 + 2;
+
+let y;
+y = 5 + 5;
+
+let z;
+z = x + y;
+
+console.log(z);
